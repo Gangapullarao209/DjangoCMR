0dc118d2c097d2163ff2d4582a1585401664bc0a
Gangapullarao209 <gangapullarao22@gmail.com>
2024-07-02 12:46:14 +0530

    initial commit

---
+++ mydb.py
@@ -0,0 +1,20 @@
+# install mysql
+# pip install mysql-connector
+# pip install mysql-connector-python
+
+
+import mysql.connector
+
+database = mysql.connector.connect(
+    host='localhost',
+    user = 'root',
+    passwd = '339061',
+
+
+)
+
+# prepare a cursor object
+cursourobject = database.cursor()
+# create a database
+cursourobject.execute("CREATE DATABASE GANGA")
+print("All Done!")

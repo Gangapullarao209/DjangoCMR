ff077efad832f3d5f3c307f2868892a2e22f51b5
Gangapullarao209 <gangapullarao22@gmail.com>
2024-07-03 11:12:53 +0530

    Final Commit

--- crmapp/forms.py
+++ crmapp/forms.py
@@ -2,20 +2,26 @@ from django.contrib.auth.forms import UserCreationForm
 
 from django.contrib.auth.models import User
 from django import forms
+from .models import Record
 
-class signupform(UserCreationForm):
+
+class SignUpForm(UserCreationForm):
         email= forms.EmailField(label="", widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Email Address'}))
         first_name=forms.CharField(label="", max_length="100",widget=forms.TextInput(attrs={'class':'form-control','placeholder':'First name'}))
         last_name=forms.CharField(label="", max_length="100",widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Last Name'}))
 
 
+        # class Meta:
+        #     model=User
+        #     fiels=('username','first_name','last_name','email','password1','password2')
+
         class Meta:
-            model=User
-            fiels=('username','first_name','last_name','email','password1','password2')
+            model = User
+            fields = ('username','first_name','last_name','email','password1','password2')
 
 
         def __init__(self, *args, **kwargs):
-            super(signupform, self).__init__(*args, **kwargs)
+            super(SignUpForm, self).__init__(*args, **kwargs)
             self.fields['username'].widget.attrs['class'] = 'form-control'
             self.fields['username'].widget.attrs['placeholder'] = 'User Name'
             self.fields['username'].label = ''
@@ -31,3 +37,19 @@ class signupform(UserCreationForm):
             self.fields['password2'].label = ''
             self.fields['password2'].help_text = '<span class="form-text text-muted"><small>Enter the same password as before, for verification.</small></span>'	
 
+
+# creating Add Record Form
+class AddRecordForm(forms.ModelForm):
+     first_name=forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"First Name","class":"form-control"}),label="")
+     last_name=forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"Last Name","class":"form-control"}),label="")
+     email=forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"Email","class":"form-control"}),label="")
+     phone=forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"Phone","class":"form-control"}),label="")
+     address=forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"Address","class":"form-control"}),label="")
+     city=forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"City","class":"form-control"}),label="")
+     state=forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"State","class":"form-control"}),label="")
+     zipcode=forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"Zipcode","class":"form-control"}),label="")
+
+     class Meta:
+          model = Record
+          exclude = ("user",)
+     
